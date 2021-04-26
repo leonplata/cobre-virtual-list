a907daeb18f9d7bda026c4adb7a377e28297be94
Leon Plata <pcg.leon@outlook.com>
2021-04-25 21:32:22 -0400

    Fixed last scenario

--- src/math.ts
+++ src/math.ts
@@ -13,6 +13,9 @@ export function calculateRange(
   if (itemsQuantity === 0 || itemHeight === 0 || itemColumns === 0 || viewportHeight === 0) {
     return { pivot: 0, length: 0 };
   }
+  if (viewportOffset >= (itemsQuantity * itemHeight)) {
+    return { pivot: itemsQuantity, length: 0 };
+  }
   const head = Math.max(0, Math.floor(viewportOffset / itemHeight));
   const tail = Math.min(itemsQuantity, Math.ceil((viewportOffset + viewportHeight)/ itemHeight));
   return { pivot: head, length: tail - head };
