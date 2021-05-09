eee2bac59e3eea5309762056410ea1d23cb2ead5
Leon Plata <pcg.leon@outlook.com>
2021-05-09 15:59:13 -0400

    Added another test

--- src/cobre-virtual-list.spec.ts
+++ src/cobre-virtual-list.spec.ts
@@ -20,4 +20,21 @@ describe('cobre-virtual-list', () => {
     expect(el.children[1].getAttribute('slot')).to.be.equal('1');
     expect(el.children[2]).to.be.undefined;
   });
+  it('should react to "items" property changes', async () => {
+    const el = await fixture<VirtualList>(html`
+      <cobre-virtual-list
+        style="height: 48px;"
+        .items=${[1, 2, 3, 4, 5]}
+        .itemHeight=${24}
+        .itemTemplate=${(item: number, slot: string, index: number) => html`
+          <div slot=${slot}>${item} - ${index}</div>
+        `}>
+      </cobre-virtual-list>
+    `);
+    el.items = [1];
+    await new Promise(resolve => setTimeout(resolve));
+    expect(el.children[0].textContent).to.be.equal('1 - 0');
+    expect(el.children[0].getAttribute('slot')).to.be.equal('0');
+    expect(el.children[1]).to.be.undefined;
+  });
 });
