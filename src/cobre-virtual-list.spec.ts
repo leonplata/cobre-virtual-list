c1701a9287670855033373938fd107c41512bee0
Leon Plata <pcg.leon@outlook.com>
2021-05-09 13:52:18 -0400

    Added unit test

--- src/cobre-virtual-list.spec.ts
+++ src/cobre-virtual-list.spec.ts
@@ -3,10 +3,21 @@ import { VirtualList } from './cobre-virtual-list.js';
 import './cobre-virtual-list.js';
 
 describe('cobre-virtual-list', () => {
-  it('should print something', async () => {
+  it('should have 2 children if the virtual list height is twice than the item height', async () => {
     const el = await fixture<VirtualList>(html`
-      <cobre-virtual-list></cobre-virtual-list>
+      <cobre-virtual-list
+        style="height: 48px;"
+        .items=${[1, 2, 3, 4, 5]}
+        .itemHeight=${24}
+        .itemTemplate=${(item: number, slot: string, index: number) => html`
+          <div slot=${slot}>${item} - ${index}</div>
+        `}>
+      </cobre-virtual-list>
     `);
-    expect(el).to.be.instanceOf(VirtualList);
+    expect(el.children[0].textContent).to.be.equal('1 - 0');
+    expect(el.children[0].getAttribute('slot')).to.be.equal('0');
+    expect(el.children[1].textContent).to.be.equal('2 - 1');
+    expect(el.children[1].getAttribute('slot')).to.be.equal('1');
+    expect(el.children[2]).to.be.undefined;
   });
-}); 
+});
